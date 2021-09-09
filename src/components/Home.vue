1ce382db228f74397394593deee3a91357af3724
Marcin Jaszczuk <martim0t0@gmail.com>
2021-09-09 15:51:46 -0500

    Intial Commit

---
+++ src/components/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <q-page class="flex flex-center">
+    <img alt="blank shirt" src="../assets/tshirt1.jpeg" style="width: 30%; padding: 1%">
+
+    <img alt="blank shirt" src="../assets/tshirt2.jpeg" style="width: 30%; padding: 1%">
+
+    <img alt="blank shirt" src="../assets/tshirt3.jpeg" style="width: 30%; padding: 1%">
+
+  </q-page>
+</template>
+
+<style>
+</style>
+
+<script>
+export default {
+  name: 'Home'
+}
+</script>
